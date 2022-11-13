1f6881a74d981f85e9370f16221ff1265dd56fee
lley154 <lawrence.ley@contextsolutions.ca>
2022-11-13 13:23:47 -0500

    - added production validation testing results

--- scripts/cardano-cli/mainnet/global-export-variables.sh
+++ scripts/cardano-cli/mainnet/global-export-variables.sh
@@ -23,3 +23,5 @@ export DONOR_ADDR=addr1v9wjlg0e9sz3q5tl4cxu3gaurasdvuvnw5fmnhwefyngphcpugugh
 export REFUND_ADDR=addr1v9x5aqdztj3t4356p58cny9zqv8vwggsu4sqraycrat385s8nkxje
 export VAL_REF_SCRIPT=0a87acbce1a58baa209c6676ab4e0c0afa16d7f53f7fcb36b7a5d2f004f63b43#1
 export SPLIT=99
+export NEXT_PUBLIC_SHOP="https://test1-2803.myshopify.com/"   # Shopify store URL
+export NEXT_PUBLIC_ACCESS_TOKEN="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"
